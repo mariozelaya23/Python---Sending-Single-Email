ff24c53534ab3986959d288cb99b72fbe264b5c3
Mario Zelaya <61225109+mariozelaya23@users.noreply.github.com>
2021-11-11 23:52:43 +0000

    add variables and set email password

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import yagmail
+import os
 
+sender = 'app7flask@gmail.com'
+reveiver = 'lzykszamb@supere.ml'
+
+subject = """
+This is the subject
+"""
+
+contents = """
+Here is the content of the email!
+Hi!
+"""
+
+yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD'))
