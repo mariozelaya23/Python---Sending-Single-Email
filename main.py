5d14585a932718bf29247ad7843d52eab4cf5870
Mario Zelaya <61225109+mariozelaya23@users.noreply.github.com>
2021-11-12 00:37:38 +0000

    updating email and password

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import yagmail
 import os
 
-sender = 'app7flask@gmail.com'
+sender = 'leadgeneratorsgurus@gmail.com'
 receiver = 'lzykszamb@supere.ml'
 
 subject = """
@@ -13,6 +13,6 @@ Here is the content of the email!
 Hi!
 """
 
-yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD'))
+yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD2'))
 yag.send(to=receiver, subject=subject, contents=contents)
 print("Email Sent!")
