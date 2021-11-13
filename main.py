16818bee8217f98fcbf97ee0211a5a25046b83da
Mario Zelaya <61225109+mariozelaya23@users.noreply.github.com>
2021-11-13 04:55:13 +0000

    add if condition setting an specific time to send an email

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import yagmail
 import os
 import time
+from datetime import datetime as dt
 
 sender = 'leadgeneratorsgurus@gmail.com'
 receiver = 'lzykszamb@supere.ml'
@@ -14,7 +15,9 @@ Here is the content of the email!
 Hi!
 """
 while True:
-  yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD2'))
-  yag.send(to=receiver, subject=subject, contents=contents)
-  print("Email Sent!")
-  time.sleep(60)
+  now = dt.now()
+  if now.hour == 13 and now.minute == 18: 
+    yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD2'))
+    yag.send(to=receiver, subject=subject, contents=contents)
+    print("Email Sent!")
+    time.sleep(60)
