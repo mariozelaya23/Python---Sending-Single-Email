e2bb17dbb0f69fd8b6aac1f8d781cef31b16c72d
Mario Zelaya <61225109+mariozelaya23@users.noreply.github.com>
2021-11-14 01:01:58 +0000

    add bills txt files, add columns to csv file, change code to attach files and send the emails from the csv file

--- main.py
+++ main.py
@@ -4,39 +4,17 @@ import pandas
 
 
 sender = 'leadgeneratorsgurus@gmail.com'
-receiver = 'lzykszamb@supere.ml'
 
-subject = """
-This is the subject
-"""
-
-contents ["""
-Here is the content of the email
-""", 'text.txt']
-
-yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD2'))
-yag.send(to=receiver, subject=subject, contents=contents)
-print("Email Sent!")
-
-
-"""
 yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD2'))
 
 df = pandas.read_csv('contacts.csv')
 #print(df)
 
 for index, row in df.iterrows():
-    contents = f"""Hi {row['name']} Here is the content of the email!"""
-    yag.send(to=row['email'], subject=subject, contents=contents)
-    print("Email Sent!")
-"""
+  receiver_email = row['email']
+  subject = "This is the subject"
+  contents = [f"""Hi, {row['name']} you have to pay {row['amount']}
+  bill is attached""", row['filepath'],]
+  yag.send(to=row['email'], subject=subject, contents=contents)
+  print("Email Sent!")
 
-"""
-while True:
-  now = dt.now()
-  if now.hour == 13 and now.minute == 18: 
-    yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD2'))
-    yag.send(to=receiver, subject=subject, contents=contents)
-    print("Email Sent!")
-    time.sleep(60)
-"""
